110addf34fda36da5d4908db8d719ded6c2b632d
namistarr <namistarr@icloud.com>
2019-06-18 14:10:15 -0700

    wip

--- js/index.js
+++ js/index.js
@@ -13,19 +13,55 @@ imgDelete.addEventListener('dblclick', event => {
 });
 
 
-// 3:
+// 3: Makes images black and white on mouseover
 const grayScale = document.querySelector('.img-content')
 grayScale.addEventListener('mouseover', event => {
     grayScale.style.filter = 'grayscale(1)';    
   });
 
+  
+    // grayScale.addEventListener("mouseenter", event => {
+    //     grayScale.style.filter = 'grayscale(1)';
+    // });
+  
+    //  item.addEventListener("mouseleave", event => {
+    //     grayScale.style.filter = 'grayscale(0)';
+    // });
+  
 
-// 4:
+// 4: Gets mad when you scroll too much
+let counter = 0;
+window.addEventListener("scroll", (event) => {
+        counter++;
+    if (counter > 150) {
+        alert("Stop moving!!"); 
+        counter = 0; 
+    }
+});
 
-// 5:
+// 5: Turns paragraph text red
+let paragraph = document.querySelector('p')
+paragraph.addEventListener('click', (event) => {
+    event.target.style.color = 'red'
+ });
 
-// 6:
-
+// 6: Makes links unclickable and hover turns hot pink
+const navLinks = document.querySelectorAll(".nav-link");
+navLinks.forEach(function (item) {
+    linkHover(item);
+    item.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+  });
+  function linkHover(item) {
+    item.addEventListener("mouseenter", (event) => {
+      item.style.color = "hotpink";
+    });
+  
+     item.addEventListener("mouseleave", (event) => {
+      item.style.color = black;
+    });
+  }
 // 7:
 
 // 8:
