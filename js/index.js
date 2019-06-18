e947733ed71c263295eb19d6c9c2c17583a2521e
namistarr <namistarr@icloud.com>
2019-06-18 13:26:37 -0700

    added first 3 addEventListener's

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+
+// 1: Message when page loads
+window.addEventListener('load', function(event) {
+    alert('Hi!');
+});
+
+// 2: Double click on images to make them disappear
+const imgDelete = document.querySelector('.img-content');
+
+imgDelete.addEventListener('dblclick', event => {
+   imgDelete.style.display = 'none';
+});
+
+
+// 3:
+const grayScale = document.querySelector('.img-content')
+grayScale.addEventListener('mouseover', event => {
+    grayScale.style.filter = 'grayscale(1)';    
+  });
+
+
+// 4:
+
+// 5:
+
+// 6:
+
+// 7:
+
+// 8:
+
+// 9:
+
+// 10:
